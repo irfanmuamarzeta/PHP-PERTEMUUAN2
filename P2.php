0894649566c55190a2175ff41a43c4b357c71089
irfanmuamarzeta <53320813+irfanmuamarzeta@users.noreply.github.com>
2019-10-09 09:05:11 +0700

    P2.php

---
+++ P2.php
@@ -0,0 +1,33 @@
+<?php
+	 // LOOPING/ PENGULANAGAN
+	//type 1=for
+	
+	//for ($i=0; $i < 10; $i++) { 
+		# code...
+	//}
+	//tyype2 = WHILE
+	//$i = 1;
+	//while( $i <=10){
+		//echo $i . "irfanmz <br>";
+		//$i++;
+		//}
+
+	//type 3 = foreach
+
+	//pengkondisian
+	/*
+	$angka = 20;
+	if ($angka >10){
+		echo "lebih besar dari 10";
+	}else if ($angka ==10){
+		echo "sama dengan 10";
+	}else{
+		echo "kurang dari 10";
+	}
+	
+	
+
+
+	
+
+?>
